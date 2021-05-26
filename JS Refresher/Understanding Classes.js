845bc47d56ae2b0481dc12814faa80375361af27
Eli Jaghab <55901654+EliJaghab@users.noreply.github.com>
2021-05-26 15:32:19 -0400

    classes. properties and methods

--- JS Refresher/Understanding Classes.js
+++ JS Refresher/Understanding Classes.js
@@ -1,19 +1,16 @@
 class Human {
-  constructor() {
-    this.gender = "male";
+    gender = "male";
   }
 
-  printGender() {
+  printGender = () => {
     console.log(this.gender);
   }
 }
 class Person extends Human {
-  constructor() {
-    super(); //Execute Parent constructor of human
-    this.name = "Max";
-    this.gender = "female";
+    name = "Max";
+    gender = "female";
   }
-  printMyName() {
+  printMyName = () => {
     console.log(this.name);
   }
 }
